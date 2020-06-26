e32cd6ebdb1c340f486011e01608205d2de7caf4
Karl Eyre <karl_eyre@hotmail.com>
2020-06-27 01:16:46 +1000

    boundary Reset Added

---
+++ Assets/PlayerFellOff.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFellOff : MonoBehaviour
+{
+    // hold player possition for reset
+    public Vector3 startPos;
+
+    void Start()
+    {
+        // set player possition for reset
+        startPos = this.gameObject.transform.position;
+        print(startPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    //reset player on colission
+    private void OnTriggerEnter2D(Collider2D x)
+    {
+        if (x.gameObject.tag == "boundry")
+        {
+        this.transform.position = startPos;
+        print("You fell off the map. Learn to jump better");
+        }
+
+    }
+
+}
