9cd7766dc40abc3187974c732a7a402eb11807fb
Karl Eyre <karl_eyre@hotmail.com>
2020-06-29 11:59:48 +1000

    parallax added. Still needs testing

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    //Refference https://www.youtube.com/watch?v=zit45k6CUMk
+
+    private float length, startpos;
+    public GameObject camera1;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (camera1.transform.position.x * (1 - parallaxEffect)); 
+        float dist = (camera1.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
